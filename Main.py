d8d90461708b081988763dfe7afab30fcab20835
riteshsookun <43849655+riteshsookun@users.noreply.github.com>
2020-03-02 20:39:59 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,23 +1,30 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
+import time
+
+
+class RawData:
+    def __init__(self, stock, start, end):
+        # Retrieve stock data
+        self.stock_data = yf.download(stock, start, end)
+
 
 #store required inputs
 stock_code = input ("Enter stock code: ")
 start_date = input ("Enter start date: ")
 end_date = input ("Enter end date: ")
 
-# Retrive stock data
-stock_data = yf.download(stock_code , start_date, end_date)
+stock_1 = RawData(stock_code, start_date, end_date)
 
 # Calculate moving average
-mavg = stock_data['Adj Close'].rolling(window=100).mean()
+mavg = stock_1['Adj Close'].rolling(window=100).mean()
 
-plt.plot(stock_data["Open"]) # Plot Open
-plt.plot(stock_data["High"]) # Plot High
-plt.plot(stock_data["Low"]) # Plot Low
-plt.plot(stock_data["Close"]) # Plot Close
+plt.plot(stock_1["Open"]) # Plot Open
+plt.plot(stock_1["High"]) # Plot High
+plt.plot(stock_1["Low"]) # Plot Low
+plt.plot(stock_1["Close"]) # Plot Close
 plt.plot(mavg) # Plot Moving Average
-plt.title('{} stock price'.format(stock_code.upper())) # Graph title
+plt.title('{} stock price'.format(stock_1.upper())) # Graph title
 plt.ylabel('Price')  # Y-axis label
 plt.legend(['Open','High','Low','Close','Moving Avg'], loc='upper left') #Legend
 plt.show() #Display graph
