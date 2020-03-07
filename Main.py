04613618dfc778d139b84f9097822877f7587299
riteshsookun <43849655+riteshsookun@users.noreply.github.com>
2020-03-07 22:25:05 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,9 @@
 import yfinance as yf
-import matplotlib.pyplot as plt
+import numpy as np
 import time
+import pickle
+from matplotlib import pyplot as plt
+
 
 class DataSet:
     stock_name = None
