299c889a1a5991f815991efdc7edffbfd58f18f1
riteshsookun <43849655+riteshsookun@users.noreply.github.com>
2020-04-15 23:50:31 +0100

    Update regression.py

--- regression.py
+++ regression.py
@@ -7,6 +7,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.svm import SVR
 from sklearn.model_selection import train_test_split
 from matplotlib import pyplot as plt
+import socket
 
 #global var to store stock code user wants to analyse using regression
 stock_name = "MSFT"
@@ -16,6 +17,8 @@ lr_df = "a"
 svm_df = "a"
 #global var to store confidence level
 confidence = 0
+lr_confidence = 0
+svm_confidence = 0
 
 
 class parameters:
@@ -106,17 +109,20 @@ class prepare_data():
 
     def determine_confidence(self):
         global confidence
+        global lr_confidence
+        global svm_confidence
 
         #SVM confidence
-        self.svm_confidence = self.svr_rbf.score(self.x_test, self.y_test)
+        svm_confidence = self.svr_rbf.score(self.x_test, self.y_test)
         print("svm confidence: ", self.svm_confidence)
 
         #LR confidence
-        self.lr_confidence = self.lr.score(self.x_test, self.y_test)
+        lr_confidence = self.lr.score(self.x_test, self.y_test)
         print("lr confidence: ", self.lr_confidence)
 
 
     def generate_predictions(self):
+
         # x_prediction to be the last 'future_days' rows of the adj close column
         x_prediction = np.array(self.df.drop(['Prediction'],1))[-self.parameters_obj.future_days:]
 
@@ -124,6 +130,7 @@ class prepare_data():
 
         self.svm_prediction = self.svr_rbf.predict(x_prediction)
 
+        parameters_obj = parameters()
         #create future dates and add to dataframe
         self.future_dates = pd.bdate_range(pd.datetime.today(), periods=parameters_obj.future_days, weekmask=None)
 
@@ -139,11 +146,13 @@ class prepare_data():
     def graphing(self):
         global lr_df
         global svm_df
+        global lr_confidence
+        global svm_confidence
 
-        if self.lr_confidence > self.svm_confidence:
+        if lr_confidence > svm_confidence:
             global confidence
-            confidence = round(self.lr_confidence * 100, 2)
-            print("Linear Regression model has a ", (self.lr_confidence - self.svm_confidence) * 100, "% higher confidence")
+            confidence = round(lr_confidence * 100, 2)
+            print("Linear Regression model has a ", (lr_confidence - svm_confidence) * 100, "% higher confidence")
             print(self.lr_prediction)
             plt.plot(self.df['Adj Close'], color='black')
             plt.plot(self.future_dates, lr_df, color='orange')
@@ -151,8 +160,8 @@ class prepare_data():
             plt.xlabel("Date")
             plt.show()
         else:
-            confidence = round(self.svm_confidence * 100, 2)
-            print("Support Vector Machine has a ", (self.svm_confidence - self.lr_confidence) * 100, "% higher confidence")
+            confidence = round(svm_confidence * 100, 2)
+            print("Support Vector Machine has a ", (svm_confidence - lr_confidence) * 100, "% higher confidence")
             print(self.svm_prediction)
             plt.plot(self.df['Adj Close'], color='black')
             plt.plot(self.future_dates, svm_df, color='orange')
@@ -181,19 +190,55 @@ class Compile():
 
     #dataset_obj = DataSet()
 
+    #function to check internet conectivity by pinging Google's DNS server
+    def internet_check(self, host="8.8.8.8", port=53, timeout=3):
+        """
+        Host: 8.8.8.8 (google-public-dns-a.google.com)
+        OpenPort: 53/tcp
+        Service: domain (DNS/TCP)
+        """
+        try:
+            socket.setdefaulttimeout(timeout)
+            socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+            return True
+        except socket.error as ex:
+            return False
+
     def compile_predictions_regression(self, stock_code):
         prepare_data_obj = prepare_data()
 
         global stock_name
-        stock_name = stock_code
 
-        prepare_data_obj.retrive_dataset()
-        prepare_data_obj.manipulate_data()
-        prepare_data_obj.convert_data()
-        prepare_data_obj.split_data()
-        prepare_data_obj.build_model()
-        prepare_data_obj.determine_confidence()
-        prepare_data_obj.generate_predictions()
-        prepare_data_obj.graphing()
-        prepare_data_obj.convert_df_export()
-        # print(stock_code)
+        if self.internet_check() == 0:
+            raise Exception("No internet connectivity")
+        else:
+            stock_name = stock_code
+
+            prepare_data_obj.retrive_dataset()
+            prepare_data_obj.manipulate_data()
+            prepare_data_obj.convert_data()
+            prepare_data_obj.split_data()
+            prepare_data_obj.build_model()
+            prepare_data_obj.determine_confidence()
+            prepare_data_obj.generate_predictions()
+            prepare_data_obj.graphing()
+
+    def compile_predictions_regression_export(self, stock_code):
+        prepare_data_obj = prepare_data()
+
+        global stock_name
+
+        if self.internet_check() == 0:
+            raise Exception("No internet connectivity")
+        else:
+            stock_name = stock_code
+
+            prepare_data_obj.retrive_dataset()
+            prepare_data_obj.manipulate_data()
+            prepare_data_obj.convert_data()
+            prepare_data_obj.split_data()
+            prepare_data_obj.build_model()
+            prepare_data_obj.determine_confidence()
+            prepare_data_obj.generate_predictions()
+            prepare_data_obj.convert_df_export()
+
